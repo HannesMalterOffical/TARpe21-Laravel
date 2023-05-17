98775a5bfff490d4ce8642ccaf477d4c10e1b2cb
Hannes Malter <hannesmalter1234@gmail.com>
2023-05-17 12:22:08 +0300

    Add task view and finish tutorial

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -6,20 +6,43 @@ use App\Models\Task;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
 
 class TasksController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-    public function create(Task $newTask)
-    {
-
-    }
-    public function delete(Task $taskToDelete)
-    {
-        # code...
-    }
     public function index()
     {
-        return 'Wassup partner';
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+
+        return view('tasks', [
+            'tasks' => $tasks
+    ]);
+    }
+    public function create(Request $request): RedirectResponse
+    {
+        $validator = $request->validate([
+            'name' => 'required|max:255',
+        ]);
+
+        // if ($validator->fails()) {
+        //     return redirect('/tasks')
+        //         ->withInput()
+        //         ->withErrors($validator);
+        // }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/tasks');
+    }
+    public function delete($taskToDelete)
+    {
+        Task::findOrFail($taskToDelete)->delete();
+        return redirect('/tasks');
     }
 }
+
